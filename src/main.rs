0761e919ad1b09906a6c38d4f70ee9910563afa9
itljan30 <itljan3012002@gmail.com>
2025-02-07 00:38:56 -0500

    Implemented the basic command line args structure

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ enum Argument {
     Length(usize),
     Height(usize),
     MaxShapes(usize),
-    PuzzleNumber(u32),
+    PuzzleNumber(usize),
+    TotalSolutions(usize),
 }
 
 fn get_avg(args: &Vec<Argument>) {
@@ -19,6 +20,7 @@ fn get_avg(args: &Vec<Argument>) {
     let mut length = 5;
     let mut height = 5;
     let mut max_shapes = 10;
+    let mut total_solutions = u32::MAX as usize;
 
     for arg in args {
         match arg {
@@ -26,13 +28,14 @@ fn get_avg(args: &Vec<Argument>) {
             Argument::Height(val) => height = *val,
             Argument::MaxShapes(val) => max_shapes = *val,
             Argument::PuzzleNumber(val) => number_of_puzzles = *val,
+            Argument::TotalSolutions(val) => total_solutions = *val,
         }
     }
 
     let mut s = Vec::new();
     for i in 0..number_of_puzzles {
         let mut puzzle = Puzzle::get_random(length, height, max_shapes);
-        let solutions = puzzle.shuffle().get_solutions(u32::MAX as usize);
+        let solutions = puzzle.shuffle().get_solutions(total_solutions);
         s.push(solutions.len());
 
         print!("\rProgress: {:>7}/{} : {:>5.2}%",i, number_of_puzzles, i as f32 / number_of_puzzles as f32 * 100.0);
@@ -40,8 +43,27 @@ fn get_avg(args: &Vec<Argument>) {
     println!("\rProgress:{:>7}/{} : 100.00%\nAverage solutions with at most {} unique shapes across {} puzzles: {}", number_of_puzzles, number_of_puzzles, max_shapes, number_of_puzzles, s.iter().sum::<usize>() as f32 / s.len() as f32);
 }
 
-fn two_solutions(args: &Vec<Argument>) {
+fn two_solutions(_args: &Vec<Argument>) {
+    eprintln!("Not Implemented: twosolutions");
+}
 
+fn display_help_info() {
+    eprintln!("Commands:
+    average              => Generates puzzles and calculates the average number of solutions per puzzle
+    twosolutions         => Generates puzzles and writes all puzzles with two solutions to a file
+Options: 
+    --help          | -h => Displays this help page
+    --version       | -v => Displays the current version
+    --length        | -x => Sets the length of the puzzles generated
+    --height        | -y => Sets the height of the puzzles generated
+    --max-shapes    | -s => Sets the maximum amount of unique shapes per puzzle
+    --puzzle-count  | -p => Sets the number of puzzles generated
+    --file          | -f => Sets the file jiggysaw will write to
+    --max-solutions | -S => Sets the max number of solutions it will search for")
+}
+
+fn display_version_info() {
+    eprintln!("JiggySaw v0.1.0")
 }
 
 fn main() {
@@ -52,7 +74,15 @@ fn main() {
         match arg.as_str() {
             "average" => functions.push(Func::Average),
             "twosolutions" => functions.push(Func::TwoSolutions),
-            "--length" | "-l" => {
+            "--version" | "-v" => {
+                display_version_info();
+                std::process::exit(0);
+            }
+            "--help" | "-h" => {
+                display_help_info();
+                std::process::exit(0);
+            }
+            "--length" | "-x" => {
                 if let Some(val) = args.next() {
                     if let Ok(num) = val.parse::<usize>() {
                         context.push(Argument::Length(num));
@@ -67,7 +97,7 @@ fn main() {
                     std::process::exit(1);
                 }
             }
-            "--height" | "-h" => {
+            "--height" | "-y" => {
                 if let Some(val) = args.next() {
                     if let Ok(num) = val.parse::<usize>() {
                         context.push(Argument::Height(num));
@@ -82,7 +112,7 @@ fn main() {
                     std::process::exit(1);
                 }
             }
-            "--max-shapes" | "-m" => {
+            "--max-shapes" | "-s" => {
                 if let Some(val) = args.next() {
                     if let Ok(num) = val.parse::<usize>() {
                         context.push(Argument::MaxShapes(num));
@@ -97,10 +127,25 @@ fn main() {
                     std::process::exit(1);
                 }
             }
-            "--total-puzzles" | "-p" => {
+            "--puzzle-count" | "-p" => {
                 if let Some(val) = args.next() {
-                    if let Ok(num) = val.parse::<u32>() {
+                    if let Ok(num) = val.parse::<usize>() {
                         context.push(Argument::PuzzleNumber(num));
+                    }
+                    else {
+                        eprintln!("Unable to parse value: {} {}", arg, val);
+                        std::process::exit(1);
+                    }
+                }
+                else {
+                    eprintln!("Value was not given for argument: {}", arg);
+                    std::process::exit(1);
+                }
+            }
+            "--total-solutions" | "-t" => {
+                if let Some(val) = args.next() {
+                    if let Ok(num) = val.parse::<usize>() {
+                        context.push(Argument::TotalSolutions(num));
                     }
                     else {
                         eprintln!("Unable to parse value: {} {}", arg, val);
@@ -117,6 +162,11 @@ fn main() {
                 std::process::exit(1);
             }
         }
+    }
+
+    if functions.len() == 0 {
+        display_help_info();
+        std::process::exit(0);
     }
 
     for function in functions {
